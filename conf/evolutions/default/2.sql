ca4a3c3a4c9724b2cd37d73b488f42d9089f6404
dhydrated <dhydrated@gmail.com>
2012-12-16 00:39:41 +0800

    fixed 2.sql

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,14 +1,7 @@
 # --- !Ups
 
-ALTER TABLE payments ADD COLUMN year integer;
-ALTER TABLE payments ADD COLUMN month integer;
-
 insert into users (id,name,email,password) values (nextval('users_seq'),'dhydrated','dhydrated@gmail.com','payupnow');
 
 # --- !Downs
-
-
-ALTER TABLE payments DROP COLUMN year integer;
-ALTER TABLE payments DROP COLUMN month integer;
 
 delete from users;

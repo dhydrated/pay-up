8a5dc2097e98a64c4897c048af3c4c4826df1946
dhydrated <dhydrated@gmail.com>
2012-12-16 23:48:29 +0800

    updated payees list

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -17,9 +17,19 @@ public class IntegrationTest {
     public void test() {
         running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
-                browser.goTo("http://localhost:3333");
+            	
+                browser.goTo("http://localhost:3333/login");
+
+                assertThat(browser.$("form h1").first().getText()).isEqualTo("Sign in");
                 
-                assertThat(browser.$("header h1").first().getText()).isEqualTo("Play 2.0 sample application — Computer database");
+                browser.$("email").text("dhydrated@gmail.com");
+                browser.$("password").text("payupnow");
+                
+                browser.$(".btn").click();
+                
+                assertThat(browser.$(".brand").first().getText()).contains("Pay Up!");
+                
+               /* assertThat(browser.$("header h1").first().getText()).isEqualTo("Play 2.0 sample application — Computer database");
                 assertThat(browser.$("section h1").first().getText()).isEqualTo("574 computers found");
 
                 assertThat(browser.$("#pagination li.current").first().getText()).isEqualTo("Displaying 1 to 10 of 574");
@@ -66,7 +76,7 @@ public class IntegrationTest {
                 browser.$("#searchsubmit").click();
 
                 assertThat(browser.$("section h1").first().getText()).isEqualTo("12 computers found");
-                
+                */
             }
         });
     }

74d22a67b9c043328694f3317197d7841891112b
dhydrated <dhydrated@gmail.com>
2012-12-25 23:40:49 +0800

    updated payment list to show month name instead of number

--- app/models/Payment.java
+++ app/models/Payment.java
@@ -2,6 +2,7 @@ package models;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -10,7 +11,9 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
+import play.Logger;
 import play.data.format.Formats;
 import play.data.validation.Constraints;
 import play.db.ebean.Model;
@@ -20,94 +23,117 @@ import com.avaje.ebean.Page;
 /**
  * Payment entity managed by Ebean
  */
-@Entity 
-@Table(name="payments")
+@Entity
+@Table(name = "payments")
 public class Payment extends Model {
 
-    @Id
-    public Long id;
+	@Id
+	public Long id;
 
-    public String name;
+	public String name;
 
-    @Constraints.Required
-    public BigDecimal amount;
-    
-    @Constraints.Max(value=9999)
-    @Constraints.Required
-    public Integer year;
-    
-    @Constraints.Max(value=12)
-    @Constraints.Required
-    public Integer month;
+	@Constraints.Required
+	public BigDecimal amount;
 
-    @Constraints.MaxLength(value=500)
-    public String remarks;
-    
-    public String reference;
+	@Constraints.Max(value = 9999)
+	@Constraints.Required
+	public Integer year;
 
-    @Formats.DateTime(pattern="yyyy-MM-dd")
-    @Column(name="paid_date")
-    public Date paidDate = new Date();
+	@Constraints.Max(value = 12)
+	@Constraints.Required
+	public Integer month;
 
-    @Constraints.Required
-    @ManyToOne
-    public Payee payee;
+	@Constraints.MaxLength(value = 500)
+	public String remarks;
 
-    @Constraints.Required
-    @ManyToOne
-    public PaymentType paymentType;
-    
-    /**
-     * Generic query helper for entity Payment with id Long
-     */
-    public static Finder<Long,Payment> find = new Finder<Long,Payment>(Long.class, Payment.class); 
-    
-    /**
-     * Return a page of payment
-     *
-     * @param page Page to display
-     * @param pageSize Number of payments per page
-     * @param sortBy Payment property used for sorting
-     * @param order Sort order (either or asc or desc)
-     * @param filter Filter applied on the name column
-     */
-    public static Page<Payment> page(int page, int pageSize, String sortBy, String order, String filter) {
-        return 
-            find.where()
-                .ilike("payee.name", "%" + filter + "%")
-                .orderBy(sortBy + " " + order)
-                .fetch("payee")
-                .findPagingList(pageSize)
-                .getPage(page);
-    }
+	public String reference;
 
-    public static Map<String,String> yearOptions() {
-        LinkedHashMap<String,String> options = new LinkedHashMap<String,String>();
+	@Formats.DateTime(pattern = "yyyy-MM-dd")
+	@Column(name = "paid_date")
+	public Date paidDate = new Date();
 
-        options.put("2013", "2013");
-        options.put("2012", "2012");
-        
-        return options;
-    }
+	@Constraints.Required
+	@ManyToOne
+	public Payee payee;
 
-    public static Map<String,String> monthOptions() {
-        LinkedHashMap<String,String> options = new LinkedHashMap<String,String>();
+	@Constraints.Required
+	@ManyToOne
+	public PaymentType paymentType;
 
-        options.put("1", "January");
-        options.put("2", "February");
-        options.put("3", "March");
-        options.put("4", "April");
-        options.put("5", "May");
-        options.put("6", "June");
-        options.put("7", "July");
-        options.put("8", "August");
-        options.put("9", "September");
-        options.put("10", "October");
-        options.put("11", "November");
-        options.put("12", "December");
-        
-        return options;
-    }
-    
+	@Transient
+	private String monthName;
+
+	@Transient
+	public static Map<String, String> months;
+
+	/**
+	 * Generic query helper for entity Payment with id Long
+	 */
+	public static Finder<Long, Payment> find = new Finder<Long, Payment>(
+			Long.class, Payment.class);
+
+	/**
+	 * Return a page of payment
+	 * 
+	 * @param page
+	 *            Page to display
+	 * @param pageSize
+	 *            Number of payments per page
+	 * @param sortBy
+	 *            Payment property used for sorting
+	 * @param order
+	 *            Sort order (either or asc or desc)
+	 * @param filter
+	 *            Filter applied on the name column
+	 */
+	public static Page<Payment> page(int page, int pageSize, String sortBy,
+			String order, String filter) {
+		return find.where().ilike("payee.name", "%" + filter + "%")
+				.orderBy(sortBy + " " + order).fetch("payee")
+				.findPagingList(pageSize).getPage(page);
+	}
+
+	public static Map<String, String> yearOptions() {
+		LinkedHashMap<String, String> options = new LinkedHashMap<String, String>();
+
+		options.put("2013", "2013");
+		options.put("2012", "2012");
+
+		return options;
+	}
+
+	public static Map<String, String> monthOptions() {
+
+		return getMonths();
+	}
+
+	public static Map<String, String> getMonths() {
+
+		if (months == null) {
+			months = new LinkedHashMap<String, String>();
+			months.put("1", "January");
+			months.put("2", "February");
+			months.put("3", "March");
+			months.put("4", "April");
+			months.put("5", "May");
+			months.put("6", "June");
+			months.put("7", "July");
+			months.put("8", "August");
+			months.put("9", "September");
+			months.put("10", "October");
+			months.put("11", "November");
+			months.put("12", "December");
+		}
+
+		return months;
+	}
+
+	public String getMonthName(){
+		
+		if(this.monthName == null){
+			this.monthName = getMonths().get(this.month.toString());
+		}
+		return this.monthName;
+	}
+
 }
-

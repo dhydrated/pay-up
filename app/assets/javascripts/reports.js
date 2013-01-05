a89393aa5fe777433f32ff9949e93f2b0f88c47b
dhydrated <dhydrated@gmail.com>
2013-01-05 21:20:30 +0800

    fixed report edit form

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -31,7 +31,40 @@ $(document).ready(function() {
 		},
 		renderChart : function() {
 
+			 // Load the Visualization API and the piechart package.
+//		      google.load('visualization', '1.0', {'packages':['corechart']});
+
+		      // Set a callback to run when the Google Visualization API is loaded.
+//		      google.setOnLoadCallback(drawChart);
+
+		      
 		},
+		// Callback that creates and populates a data table,
+	      // instantiates the pie chart, passes in the data and
+	      // draws it.
+      drawChart: function() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 3],
+          ['Onions', 1],
+          ['Olives', 1],
+          ['Zucchini', 1],
+          ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'How Much Pizza I Ate Last Night',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      },
 		fetchData : function() {
 
 			this.data = new PayUp.Collections.ReportDatas();
@@ -39,6 +72,7 @@ $(document).ready(function() {
 			this.data.fetch({
 				success : function(data) {
 					console.log('success');
+					that.renderChart();
 				},
 				error : function() {
 					console.log('error');

d62a87e0c71e5a307b4e8fa5438b80a99924f134
dhydrated <dhydrated@gmail.com>
2013-09-16 22:45:37 +0800

    fix the sql to update pwd

--- conf/evolutions/default/23.sql
+++ conf/evolutions/default/23.sql
@@ -1,7 +1,7 @@
 # --- !Ups
 create table credentials_backup as table credentials;
 delete from credentials where id not in (select credential_id from users u join credentials c on u.credential_id = c.id);
-update credentials set password =  md5(password)
+update credentials set password =  md5(password);
 
 
 # --- !Downs

051f248658b3345576c0b965ca6def61908f9cd2
dhydrated <dhydrated@gmail.com>
2013-11-10 17:29:02 +0800

    dynamically added reference field when month is selected in monthly payment page

---
+++ app/assets/javascripts/reference-controller.js
@@ -0,0 +1,16 @@
+function ReferenceController($scope) {
+	
+	/*$scope.updateReferenceFields = function(value) {
+		console.log('hi there!');
+		console.log(value);
+		console.log($scope);
+		
+		var obj = angular.element("#references["+value+"]");
+		console.log(obj);
+		obj.css( "display", "inline");
+	};
+	
+	$scope.toggle = function(a){
+		console.log(a);
+	}*/
+}

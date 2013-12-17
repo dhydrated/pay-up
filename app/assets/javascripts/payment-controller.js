6a92bbaf0a6fb6548714e833cc3a2502f47d71d3
Taufek Johar <taufek.johar@my.experian.com>
2013-12-17 08:52:23 +0800

    change the paymentType to object in angular

--- app/assets/javascripts/payment-controller.js
+++ app/assets/javascripts/payment-controller.js
@@ -83,7 +83,7 @@ app.directive("paymentTemplates", function($http){
 				scope.$parent['payment']['payer'] = template['payer'];
 				scope.$parent['payment']['payee'] = template['payee'];
 				scope.$parent['payment']['payeeAccountNumber'] = template['payeeAccountNumber'];
-				scope.$parent['payment']['paymentTypeId'] = template['paymentType']['id'];
+				scope.$parent['payment']['paymentType']['id'] = template['paymentType']['id'];
 
 				$('#templates-list-modal').modal('hide');
 				window.scope = scope.$parent;
@@ -111,7 +111,9 @@ app.controller('CreatePaymentController', function($scope, $http) {
 			id: "",
 			name: ""
 		},
-		paymentTypeId: "",
+		paymentType: {
+			id: ""
+		},
 		startPeriod: Date.today().clearTime().moveToFirstDayOfMonth().toString('dd/MM/yyyy'),
 //		startPeriod: "2012-09-01T00:00:00.000Z",
 		endPeriod: ""

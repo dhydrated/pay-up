3b9579c2db7c969cc6d3da8297c88ad45f486489
dhydrated <dhydrated@gmail.com>
2014-01-04 18:04:17 +0800

    update chgpwd

--- app/assets/javascripts/changepassword-controller.js
+++ app/assets/javascripts/changepassword-controller.js
@@ -8,28 +8,18 @@ app.controller('ChangePasswordController', function($scope, $http) {
 		confirm_password : ""
 	};
 	
-	$scope.changePassword = function(e){
+	$scope.notTheSame = function(){
+		if($scope.data.first_password !== $scope.data.confirm_password){
+			return true;
+		}
 		
-
+		return false;
+	};
+	
+	$scope.changePassword = function(e){
 		
 		if($scope.data.first_password !== '' &&
 				$scope.data.first_password === $scope.data.confirm_password){
-			
-			/*var post_data = {
-					password: $scope.data.first_password
-			}
-			
-			console.log(post_data);
-			
-			$http
-			.post('/users/loggedin_user_update_password', post_data)
-			.success(function(data, status, headers, config) {
-				console.log(status);
-			})
-			.error(function(data, status, headers, config) {
-				console.log('failed update password.');
-			});*/
-			
 			
 		}
 		else{
